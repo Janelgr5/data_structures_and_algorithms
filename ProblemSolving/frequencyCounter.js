bebd3d40a8d90e9c15963bd2d46dbcc4c8ac2dfa
Janel Grant <janelgr5@gmail.com>
2019-11-11 15:36:59 -0500

    frequencyCounter sameFrequencyproblem

---
+++ ProblemSolving/frequencyCounter.js
@@ -0,0 +1,42 @@
+// Write a function called sameFrequency.Given two positive integers, find out if the two numbers hae the same frequency of digits.
+
+function sameFrequency(num1, num2) {
+  // turn numbers into strings
+  const num1Str = String(num1),
+    num2Str = String(num2);
+  // if numbers are different lengths then they do not have the same frequency, so we can return false.
+  if (num1Str.length !== num2Str.length) {
+    return false;
+  }
+  // create objects to keep track of frequency count
+  let num1Obj = {},
+    num2Obj = {};
+  // loop through the string and create frequency count objects
+  for (let i = 0; i < num1Str.length; i++) {
+    if (num1Obj[num1Str[i]]) {
+      num1Obj[num1Str[i]] += 1;
+    } else {
+      num1Obj[num1Str[i]] = 1;
+    }
+
+    if (num2Obj[num2Str[i]]) {
+      num2Obj[num2Str[i]] += 1;
+    } else {
+      num2Obj[num2Str[i]] = 1;
+    }
+  }
+
+  // loop through first object and check to see if both objects have the same Key/value pairs
+
+  for (let key in num1Obj) {
+    if (num1Obj[key] !== num2Obj[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(sameFrequency(182, 281)); // true
+console.log(sameFrequency(34, 14)); // false
+console.log(sameFrequency(3589578, 5879385)); // true
+console.log(sameFrequency(22, 222)); // false
